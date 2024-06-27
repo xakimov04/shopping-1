042fb8d1ccb7cb077b0403ae601598ae111768e0
Mardonbekmelsov <153820615+Mardonbekmelsov@users.noreply.github.com>
2024-06-27 11:38:24 +0500

    commit

---
+++ android/app/src/main/kotlin/com/example/dars_65/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dars_65
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:dars_65/views/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainScreen(),
+    );
+  }
+}
